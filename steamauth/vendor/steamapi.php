6c1af64a5e96526e6dde33f336a0eb342fe9bbf7
maxim <sosiskavteste5225@gmail.com>
2025-04-08 11:12:23 +0600

    fix

--- steamauth/vendor/steamapi.php
+++ steamauth/vendor/steamapi.php
@@ -40,7 +40,7 @@ try {
             $ptn = "/^http:\/\/steamcommunity\.com\/openid\/id\/(7[0-9]{15,25}+)$/";
             preg_match($ptn, $id, $matches);
 
-            // Получаем ыSteamID64в
+            // Получаем ыSteamID64а
             $steamID64 = $matches[1];
 
             // Сохраняем SteamID в сессии
